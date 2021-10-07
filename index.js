a6ba972487c12a891d19e846aaccd548f46ae123
muhammadareeb313 <areebmuhammad96@gmail.com>
2021-10-07 19:52:04 +0500

    first server

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const app = express()
+
+const port = 3000
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+
+app.get('/profile', (req, res) => {
+    res.send('here is your profile')
+})
+app.post('/profile', (req, res) => {
+    res.send('profile created')
+})
+app.put('/profile', (req, res) => {
+    res.send('profile updated')
+})
+app.delete('/profile', (req, res) => {
+    res.send('profile deleted')
+})
+
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
+})
